692368405abf63a16ebfe1d82690ccc7e4586408
swadeshiBaba <bhutada.nikhil52@gmail.com>
2021-06-02 03:25:54 +0530

    First working code react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Expense from './components/Expense'
+
+function App() {
+
+    const expenses = [
+        { id:'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14)},
+        { id:'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+        { id:'e3', title: 'TV', amount: 300.64, date: new Date(2021, 1, 25)},
+        { id:'e4', title: 'New Desk Wooden', amount: 500.67, date: new Date(2021, 4, 30)}
+    ];
+
+
+  return (
+    <Expense expenses = {expenses}></Expense>
+  );
+}
+
+export default App;
