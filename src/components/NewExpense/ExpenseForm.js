02edd7ef44f8d84f1ee918a10a35e1efaed89cc6
swadeshiBaba <bhutada.nikhil52@gmail.com>
2021-06-09 03:19:24 +0530

    assignment hide unhide form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -36,7 +36,8 @@ const ExpenseForm = (props) => {
         setEnteredAmount('');
         setEnteredDate('');
     };
-    return (
+
+return (
     <form onSubmit = {submitHandler}>
         <div className= 'new-expense__controls'>
             <div className= 'new-expense__control'>
@@ -53,6 +54,7 @@ const ExpenseForm = (props) => {
             </div>
         </div>
         <div className = 'new-expense__actions'>
+            <button type='button' onClick = {props.onCancel}>Cancel</button>
             <button type='submit'>Add Expense</button>
         </div>
     </form>
