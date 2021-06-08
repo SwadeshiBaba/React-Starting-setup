53c0244dab51a262bd49bd74edbc03396192568f
swadeshiBaba <bhutada.nikhil52@gmail.com>
2021-06-09 02:10:01 +0530

    Outputting condition content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const DUMMY_EXPENSES = [{ id:'e1', title: 'Toilet Papier', amount: 94.12, date: new Date(2020, 7, 14)},
+const DUMMY_EXPENSES = [{ id:'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14)},
                         { id:'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
                         { id:'e3', title: 'TV', amount: 300.64, date: new Date(2021, 1, 25)},
                         { id:'e4', title: 'New Desk Wooden', amount: 500.67, date: new Date(2021, 4, 30)}
