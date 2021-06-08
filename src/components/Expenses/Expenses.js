9fec18946ff7c0e76a67697455eab40b886d62cb
swadeshiBaba <bhutada.nikhil52@gmail.com>
2021-06-09 01:50:45 +0530

    rendering of list data, using stateful

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,11 +14,13 @@ const Expenses = (props) => {
 
     };
 
+    const filteredExpenses = props.items.filter(expense => expense.date.getFullYear().toString() == filteredYear);
+
     return(
     <div>
         <Card className='expense'>
           <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-          {props.items.filter(expense => expense.date.getFullYear()==filteredYear).map((expense, index) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))}
+          {filteredExpenses.map((expense, index) => (<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>))}
           {/*<ExpenseItem title = {props.items[0].title} amount = {props.items[0].amount} date = {props.items[0].date}></ExpenseItem>
           <ExpenseItem title = {props.items[1].title} amount = {props.items[1].amount} date = {props.items[1].date}></ExpenseItem>
           <ExpenseItem title = {props.items[2].title} amount = {props.items[2].amount} date = {props.items[2].date}></ExpenseItem>
